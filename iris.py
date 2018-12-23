4000e6518a4593ab771547d1271003e693890b5b
aryanpankaj <aryanpankaj.jangid@gmail.com>
2018-12-23 22:52:19 +0530

    initial commit

---
+++ iris.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+#import seaborn as sns
+#import matplotlib.pyplot as plt
+#sns.set(color_codes=True)
+#%matplotlib inline
+
+df = pd.read_csv('iris.data', header=-1)
+df.head()
+print(df.head())
